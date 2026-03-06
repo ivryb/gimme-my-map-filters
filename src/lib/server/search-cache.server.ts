ff907169b1fba7c4ed27994e1feea39786a188bd
Ivan Rybnikov <ivrybn@gmail.com>
2026-03-06 19:45:45 +0800

    envs

--- src/lib/server/search-cache.server.ts
+++ src/lib/server/search-cache.server.ts
@@ -3,8 +3,8 @@ import { dirname } from 'node:path'
 import { Redis } from '@upstash/redis'
 
 const kv = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.UPSTASH_REDIS_REST_KV_REST_API_URL!,
+  token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN!,
 })
 
 export interface SearchCache<T> {
